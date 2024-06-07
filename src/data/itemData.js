becc4261beb109c5d88442db94595c98f5d5b035
realyutou <ssps9430@gmail.com>
2024-06-07 15:05:11 +0800

    feat: add itemData

---
+++ src/data/itemData.js
@@ -0,0 +1,73 @@
+const items = [
+  {
+    id: 1,
+    name: "接骨木魔杖",
+    price: 1000,
+    description: "死神的聖物之一，擁有極強力量的魔杖。",
+    image: "https://i.imgur.com/B2zcmd7.png",
+  },
+  {
+    id: 2,
+    name: "重生石",
+    price: 1000,
+    description:
+      "死神的聖物之一，能夠讓擁有人以半實體的形式把已故的親人帶回自己身邊，並能與他們溝通。",
+    image: "https://i.imgur.com/Jc2tFYk.jpg",
+  },
+  {
+    id: 3,
+    name: "隱形斗篷",
+    price: 1000,
+    description: "死神的聖物之一，具有永久性無法被識破的隱形效果。",
+    image: "https://i.imgur.com/7skYAEa.jpg",
+  },
+  {
+    id: 4,
+    name: "金探子",
+    price: 800,
+    description: "在魁地奇比賽中使用的金色小圓球，可高速飛行。",
+    image: "https://i.imgur.com/CBvNtzZ.jpg",
+  },
+  {
+    id: 5,
+    name: "劫盜地圖",
+    price: 500,
+    description:
+      "記載霍格華茲秘密通道的地圖，用魔杖指著地圖說出「我在此鄭重發誓，我絕對不懷好意」即可開啟，結束時，用魔杖指著羊皮紙說出「惡作劇完成」。",
+    image: "https://i.imgur.com/IyeBOjC.jpg",
+  },
+  {
+    id: 6,
+    name: "火閃電",
+    price: 1500,
+    description:
+      "採用流線型設計，優質白蠟木柄，鑽石打磨，註冊號碼手工鐫寫。掃帚尾部每根精心挑選的白樺樹枝都磨成流線型，使掃帚具有無與倫比的平衡性和精妙的準確性，能在十秒鐘內從靜止加速到每小時150英里。",
+    image: "https://i.imgur.com/ddj9MwE.jpg",
+  },
+  {
+    id: 7,
+    name: "柏蒂全口味豆子",
+    price: 50,
+    description:
+      "魔法世界最受歡迎的糖果之一，口味多到超乎想像，每一口都是一次冒險的經歷。",
+    image: "https://i.imgur.com/M5AuHGd.jpg",
+  },
+  {
+    id: 8,
+    name: "分類帽",
+    price: 800,
+    description:
+      "一頂有思想的霍格華茲尖頂巫師帽，會將學生分配到最適合他們的學院。",
+    image: "https://i.imgur.com/nTm5hXL.jpg",
+  },
+  {
+    id: 9,
+    name: "湯姆瑞斗的日記",
+    price: 600,
+    description:
+      "湯姆瑞斗的日記本，同時也是他的其中一個分靈體，請勿將此物品靠近蛇妖的毒牙。",
+    image: "https://i.imgur.com/O9YrjTD.jpg",
+  },
+];
+
+export { items };
